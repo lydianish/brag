7833e9e281968cf72fcee44bf4ee0c67cea3b22b
Lydia Nishimwe <nishimwelydia@yahoo.com>
2018-11-14 21:17:02 +0100

    only search when passed title is not empty

--- src/utils/impactFactor.js
+++ src/utils/impactFactor.js
@@ -1,6 +1,9 @@
 import { journals} from '../assets/samplecsv'
 
 export function impactFactor (journalTitle) {
-    const res = journals.find( (j) => j.Title.toLowerCase() === journalTitle.toLowerCase() );
-    return res ? res.SJR.replace(/,/, '.') : '';
+    if (journalTitle) {
+        const res = journals.find( (j) => j.Title.toLowerCase() === journalTitle.toLowerCase() );
+        return res ? res.SJR.replace(',' , '.') : '';
+    }
+    return '';
 }
