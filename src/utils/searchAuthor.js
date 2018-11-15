b904bcb6fd10ded749a2f51769380cbb43cbd28f
Lydia Nishimwe <nishimwelydia@yahoo.com>
2018-11-15 22:59:02 +0100

    merge conflict

--- src/utils/searchAuthor.js
+++ src/utils/searchAuthor.js
@@ -27,17 +27,14 @@ export async function searchAuthorPM (searchTerm) {
 }
 
 export async function searchAuthorGS (searchTerm) {
-<<<<<<< HEAD
     const googleScholarUrl = GOOGLE_SCHOLAR_URL;
-=======
-    const googleScholarUrl = 'http://localhost:5002/author';
->>>>>>> dev
     const searchParams = '?name='+searchTerm.replace(/\s+/g,'+');
     try {
         const response = await axios.get(googleScholarUrl + searchParams);
         return response.data;
     }
     catch(error) {
+        console.log(error)
         throw String(error);
     }
 }
