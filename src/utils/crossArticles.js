73eb7694294095bae28beb1d1348380e568844ac
lilianmh <lilian.mehll@gmail.com>
2018-11-16 17:25:21 +0100

    modif crossArticles

--- src/utils/crossArticles.js
+++ src/utils/crossArticles.js
@@ -3,28 +3,25 @@ function croisementNombreCitation(listeArticlePm,listeArticleGs)
 }
 
 
-
-
-
+var listeArticlePm = [];
+var listeArticleGs =[];
+croisementNombreCitation(listeArticlePm,listeArticleGs);
 
 function nombreCitationArticle(article,listeArticleGs){
-    try {
+
          var length=listeArticleGs.length;
          var i = 0;
          var correspondance = false;
          while ((i<length)&&(!correspondance))
              {   urlArticleGs = listeArticleGs[i].url;
 
-
-                 //on a alors le pmId de l'article google scholar, s'il corresond au pmid de l'artcle pubmed, on sort de la boucle
                 if (urlArticlesGs === PUBMED_ARTICLE_URL+article.pmid){
                     correspondance=true;}
-                     }
-                 i=i+1;
+         i=i+1;
              }
          if (i<length+1){article.citationCount = listeArticleGs[i-1].citationCount;}
 
-         else {throw Error;}
-   catch (error) {article.citationCount = undefined;}
+         else {article.citationCount = undefined;}
+
 }
 
