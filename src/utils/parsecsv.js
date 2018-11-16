105d168fbe1ee8f7a5283da0e3abfe3829f38a80
Lydia Nishimwe <nishimwelydia@yahoo.com>
2018-11-17 00:08:56 +0100

    changed journals csv to the Thomson Reuters list

--- src/utils/parsecsv.js
+++ src/utils/parsecsv.js
@@ -1,9 +1,9 @@
-import * as csv from 'fast-csv';
-import { IMPACT_FACTOR_CSV_FILE_PATH } from '../utils';
+const csv = require('fast-csv');
+const IMPACT_FACTOR_CSV_FILE_PATH = '../assets/impact-factor/sampleTR.csv';
 
 console.log("[");
 csv
- .fromPath(IMPACT_FACTOR_CSV_FILE_PATH, {headers : [ , ,"Title", , ,"SJR", , , , , , , , , , , , ,], delimiter: ';'})
+ .fromPath(IMPACT_FACTOR_CSV_FILE_PATH, {headers : [ ,"Title", ,"ImpactFactor", ,], delimiter: ';'})
  .on("data", function(data){
     console.log(" ",data);
     console.log("  ,");
